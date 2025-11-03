5de06811cb2a2c38f5d528b2dd1f5baf133d85aa
dheazkelvin <dheaz.kelvin.harahap-2020@ftmm.unair.ac.id>
2025-11-03 13:57:51 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundColor: "#f9f9f9",
+      }}
+    >
+      <h1 style={{ fontSize: "3rem", color: "#FF6600" }}>Halo ini Kelompok 2 by Othman</h1>
+      <p style={{ fontSize: "1.2rem", color: "#333" }}>Othmand made happiness</p>
+    </div>
+  );
+}
+
+export default App;
